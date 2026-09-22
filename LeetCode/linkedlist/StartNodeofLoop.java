8deaa23b354ed8e36bb86b14d6d8c3532348e65c
John <johnnitish2024@gmail.com>
2026-09-22 19:50:12 +0530

    Adding a +1 to a linkedlist

---
+++ LeetCode/linkedlist/StartNodeofLoop.java
@@ -0,0 +1,30 @@
+public class  {
+    public static int addHelper(Node temp) {
+        if (temp == null) {
+            return 1;
+        }
+        
+        int carry = addHelper(temp.next);
+        temp.data += carry;
+        
+        if (temp.data < 10) {
+            return 0;
+        }
+        
+        temp.data = 0;
+        return 1;
+    }
+
+    public static Node addOne(Node head) {
+        int carry = addHelper(head);
+        
+        if (carry == 1) {
+            Node newNode = new Node(1);
+            newNode.next = head;
+            head = newNode;
+        }
+        
+        return head;
+    }
+}
+
